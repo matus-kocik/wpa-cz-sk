0d4ead9940ab645f9a20b6aae1c2dfd8272df5c0
Matúš Kočik <matuskocik@gmail.com>
2025-01-25 12:24:50 +0100

    Initial commit

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+from pytest import fixture
+
+
+@fixture
+def num():
+    """Provide a base number for testing."""
+    return 1
---
+++ tests/test_sample.py
@@ -0,0 +1,3 @@
+def test_adding(num):
+    """Test addition using the fixture."""
+    assert num + 1 == 2
