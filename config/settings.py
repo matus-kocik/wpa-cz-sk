db9df5276e91eccbe52b2568f4559f03e944f159
Matus <matuskocik@gmail.com>
2026-01-21 17:33:39 +0100

    update settings, config change

--- config/settings.py
+++ config/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from decouple import config
+from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+config = Config(RepositoryEnv(BASE_DIR.parent / ".env"))
 
 
 # Quick-start development settings - unsuitable for production
